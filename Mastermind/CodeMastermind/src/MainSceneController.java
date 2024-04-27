f6604d985aede78ebbaddaba5f32e510e1310b5c
Bladureau <168075261+Bladureau@users.noreply.github.com>
2024-04-27 11:40:06 +0200

    Début du code pour le mastermind
    App.java pour lancer l'application
    MainSceneController.java pour controller les objets dans le SceneBuilder 
    MainScene.fxml pour relier MainSceneController.java à App.java

---
+++ Mastermind/CodeMastermind/src/MainSceneController.java
@@ -0,0 +1,16 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+
+    @FXML
+    void btnAmiClicked(ActionEvent eventAmi) {
+        
+    }
+
+    @FXML
+    void btnOrdiClicked(ActionEvent eventOrdi) {
+
+    }
+
+}
