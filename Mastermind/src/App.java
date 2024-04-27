afa23a853fb0168e411e7bfca996d4f90b4dd386
Bladureau <168075261+Bladureau@users.noreply.github.com>
2024-04-27 14:21:07 +0000

    modified:   Mastermind/src/App.java

--- Mastermind/src/App.java
+++ Mastermind/src/App.java
@@ -1,28 +1,38 @@
-import java.io.IOException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.stage.StageStyle;
 import javafx.stage.Stage;
 
 
-public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("Jeu du Mastermind");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+/*
+ * new Scene(<root>, <color>)
+ * new Scene(<root>, <weight>, <height>, <color>)
+ */
 
-            Stage otherStage = new Stage();
-            otherStage.show();
-        
-        } catch (IOException e) {
-            //empty catch
-        }
+
+
+
+public class App extends Application 
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception 
+    {
+        /*
+          * Type pour le StageStyle : 
+          * DECORATED → On garde les boutons de la barre de titre
+          * UNDECORATED → On ne garde pas les boutons de la barre de titre 
+          * TRANSPARENT → On garde les boutons de la barre de titre mais on les rend invisibles
+        */
+        // primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        Color black = Color.BLACK;      // permet de définir la couleur du fond de la page qui s'ouvre. Ex : ici elle est noir 
+        Scene sc = new Scene(new Group(), 1280.0, 720.0, black);        //new Scene(<root>, <weight>, <height>, <color>)
+        primaryStage.setScene(sc);      // Affecte la scene à la fenêtre
+        primaryStage.setTitle("MasterMind");        // Change le titre de la fenetre de l'app
+
+        primaryStage.show();            // Montre la scene
     }
 
     public static void main(String[] args) {
