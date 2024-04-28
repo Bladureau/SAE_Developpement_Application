8cdfa96ba8ee9e90308a814c86665b9a50ff0b46
Bladureau <168075261+Bladureau@users.noreply.github.com>
2024-04-28 10:04:15 +0000

    modified:   Mastermind/src/App.java 	modified:   Mastermind/src/MainSceneController.java

--- Mastermind/src/App.java
+++ Mastermind/src/App.java
@@ -1,38 +1,43 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.paint.Color;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.StageStyle;
 import javafx.stage.Stage;
 
 
-/*
- * new Scene(<root>, <color>)
- * new Scene(<root>, <weight>, <height>, <color>)
- */
+public class App extends Application {
 
-
-
-
-public class App extends Application 
-{
     @Override
-    public void start(Stage primaryStage) throws Exception 
-    {
+    public void start(Stage primaryStage) throws Exception {
+
+
         /*
           * Type pour le StageStyle : 
           * DECORATED → On garde les boutons de la barre de titre
           * UNDECORATED → On ne garde pas les boutons de la barre de titre 
           * TRANSPARENT → On garde les boutons de la barre de titre mais on les rend invisibles
         */
+        
         // primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent page1 = FXMLLoader.load(getClass().getResource("page_1.fxml"));
+        Scene scene = new Scene(page1);
+        primaryStage.setTitle("MasterMind");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-        Color black = Color.BLACK;      // permet de définir la couleur du fond de la page qui s'ouvre. Ex : ici elle est noir 
-        Scene sc = new Scene(new Group(), 1280.0, 720.0, black);        //new Scene(<root>, <weight>, <height>, <color>)
-        primaryStage.setScene(sc);      // Affecte la scene à la fenêtre
+        
+
+        /* 
+        // Color black = Color.BLACK;      // permet de définir la couleur du fond de la page qui s'ouvre. Ex : ici elle est noir 
+        scene = new Scene(new Group(), 1280.0, 720.0);        //new Scene(<root>, <weight>, <height>, <color>)
+        primaryStage.setScene(scene);      // Affecte la scene à la fenêtre
         primaryStage.setTitle("MasterMind");        // Change le titre de la fenetre de l'app
-
         primaryStage.show();            // Montre la scene
+        */
     }
 
     public static void main(String[] args) {
--- Mastermind/src/MainSceneController.java
+++ Mastermind/src/MainSceneController.java
@@ -1,16 +1,49 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import java.awt.event.*;
+import java.io.IOException;
+
+import javax.swing.*;
 
 public class MainSceneController {
 
     @FXML
-    void btnAmiClicked(ActionEvent eventAmi) {
-        
+    private Button btnAmi;
+
+    @FXML
+    private Button btnOrdi;
+
+    @FXML
+    private Button btnCommencer;
+
+    @FXML
+    private Button btnReprendre;
+
+    @FXML
+    void btnAmiClicked(ActionEvent event) throws IOException {
+        if (btnAmi.isPressed()) {
+            Parent page2Ami = FXMLLoader.load(getClass().getResource("page_2_ami.fxml"));
+            Scene scene = new Scene(page2Ami);
+        }
     }
 
     @FXML
-    void btnOrdiClicked(ActionEvent eventOrdi) {
+    void btnOrdiClicked(ActionEvent event) {
 
+    }
+
+    @FXML
+    void btnCommencerClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReprendreClicked(ActionEvent event) {
+        
     }
 
 }
