68303c3bea5db95a4a34c29164727ed4c65c8881
Bladureau <168075261+Bladureau@users.noreply.github.com>
2024-05-30 13:26:51 +0200

    Update GestionFichier.java

--- Mastermind/src/iut/info1/sae/GestionFichier.java
+++ Mastermind/src/iut/info1/sae/GestionFichier.java
@@ -3,25 +3,57 @@ package iut.info1.sae;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.json.simple.JSONObject;
+import javafx.beans.property.StringProperty;
 
 public class GestionFichier {
 
+    private StringProperty nomJoueur1;
 
-    public static void ecritureFichier() {
-        try {
-            FileWriter writer = new FileWriter("sauvergarde.txt");
+    private StringProperty nomJoueur2;
 
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+    public String getNomJoueur1() {
+        return nomJoueur1.get();
+    }
+
+    public StringProperty nomJoueur1Property() {
+        return nomJoueur1;
+    }
+
+    public String getNomJoueur2() {
+        return nomJoueur2.get();
+    }
+
+    public StringProperty nomJoueur2Property() {
+        return nomJoueur2;
     }
 
     public static void effacerFichier() {
-        File myFile = new File("C:\\Users\\bapti\\OneDrive\\Bureau\\JavaProject\\Servieres\\SAE\\Mastermind\\sauvergarde.txt");
+        File myFile = new File("sauvergarde.json");
         if(myFile.delete()) { 
             System.out.println("Fichier supprimé avec succès"); 
+        }
+    }
+
+    public static void sauvergarde(JSONObject sauvegarde) {
+        // Déclaration du FileChoser
+        File file = new File("sauvergarde.json");
+
+        if (file.exists()) {
+            GestionFichier.effacerFichier();
+        }
+
+        if (file != null) {
+            //Si l'extension n'est pas bien renseignée on l'ajoute
+            if (!file.getPath().endsWith(".json")) file = new File(file.getPath() + ".json");
+            //On écrit le fichier avec un try..catch en cas d'erreur
+            try (FileWriter fichier = new FileWriter(file)) {
+                fichier.write(sauvegarde.toJSONString());
+                fichier.flush();
+                } catch (IOException e){
+                  //On affiche l'erreur en console
+                  System.out.println(e.getMessage());
+                }
         }
     }
 }
