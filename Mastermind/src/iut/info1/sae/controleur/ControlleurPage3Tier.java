6250c1261a1d7ea6bf91f02d922a4e942ebd2da1
GS-romeo <168072805+GS-romeo@users.noreply.github.com>
2024-05-30 09:42:12 +0200

    ControlleurPage3Tier.java

--- Mastermind/src/iut/info1/sae/controleur/ControlleurPage3Tier.java
+++ Mastermind/src/iut/info1/sae/controleur/ControlleurPage3Tier.java
@@ -1,1 +1,20 @@
+package iut.info1.sae.controleur;
 
+import iut.info1.sae.Mastermind;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ControleurPage3Tier {
+
+    @FXML
+    private TextField tfJoueur1;
+
+    public TextField getNomJoueur1() {
+        return tfJoueur1;
+    }
+}
